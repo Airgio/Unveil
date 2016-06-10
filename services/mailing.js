75b7e37c1338d68b07c9dabad1f90a13c4df5ceb
Thibault <thibault@touscoprod.com>
2016-06-10 10:14:40 +0200

    add : Template mail

--- services/mailing.js
+++ services/mailing.js
@@ -1,15 +1,24 @@
 var Email = require('email').Email;
+var	fs = require('fs');
 
 module.exports = function(app){
+
 	function sendemail(data, email){
-		console.log(email);
-		var myMsg = new Email({ 
+
+		var myMsg = new Email({
 			from: 'join@unveil.com',
 			to:   email,
 			subject: data.name + " à une grande nouvelle à t'annoncer",
 			bodyType: "html",
 			body: '<table><tr><td><h1 style="color: red">HTML body</h1><a href="'+data.url+'/login">Clique ici</a></td></tr></table>'
+			//body : fs.readFile('../views/invite', 'utf8', function(){
+
+
+			//})
 		});
+		console.log(fs.readFile('/views/invite', function(err, data){
+
+			}));
 		myMsg.send();
 	}
 
