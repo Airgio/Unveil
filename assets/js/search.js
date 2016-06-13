c1af247f5dd5d14cfe80d4ff247c43effa844600
NylsSs <nilsredoulez@gmail.com>
2016-06-13 20:31:11 +0200

    style search bar contact

--- assets/js/search.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-	//var emails = !sessionStorage.emails ? null : JSON.parse(sessionStorage.emails);
-	var availableTags = emails;
-	console.log(availableTags);
-	$( "#tags" ).autocomplete({
-      source: availableTags
-    });
-});
