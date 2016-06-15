89a8bda14ec0fa0a648b09a7f2f6b86145e50ec0
NylsSs <nilsredoulez@gmail.com>
2016-06-15 13:49:47 +0200

    Refacto js

--- assets/js/script_back.js
+++ assets/js/script_back.js
@@ -1,11 +1,24 @@
-$(function(){
+var socket = io();
+var emails = !sessionStorage.emails ? null : JSON.parse(sessionStorage.emails) ;
+var url = window.location.search;
+var user_email = url.substring(url.lastIndexOf('=')+1) || 0;
+var clientId = '605751688227-u0fliiq32028oh6uim9mguae8hqfb7jj.apps.googleusercontent.com';
+var apiKey = 'AIzaSyAOpIxefwOLJi7luSaFxNNSjNn7EjJWru0';
+var scopes = 'https://www.googleapis.com/auth/contacts.readonly';
 
-	$('#chat').hide();
-	$('#unveil').hide();
+function display_contacts(){
+	// Create list of contact
+	$.each(emails, function(ind, val){
+	    $('#contact_list').append('<li data-index='+ind+'><p>'+val.title+' <span class="user_mail">'+val.email+'</span></p></li>');
+	});
+}
 
-	// Contact management
-    var _tmp_invitations = [];
+function contact_management(){
+	display_contacts();	
 
+	var _tmp_invitations = [];
+
+	// Select contacts
     $('#contact_list li').click(function(){
         // Get index of each item
         var _tmp_id = parseInt($(this).attr('data-index'));
@@ -26,22 +39,6 @@ $(function(){
         }
     });
 
-
-
-    // Create invitation
-    $('#launch_room').click(function () {
-		event.preventDefault();
-		var d = $(this).serializeObject();
-			d.attendees = JSON.parse(sessionStorage.invitations);
-			d.url = window.location.protocol +'//'+ window.location.hostname +(':'+ window.location.port || '') ;
-		socket.emit('create', d);
-		socket.emit('post', d);
-
-        sessionStorage.userEmail = d.name;
-	});
-
-
-
     // BACK TO THE ROOM CREATION
 	$('#back').click(function(){
 		var _invitations = [];
@@ -61,9 +58,38 @@ $(function(){
 		event.preventDefault();
 	});
 
+} // contact_managemnt
+
+function create_room(){
+
+	// Create invitation
+    $('#launch_room').click(function () {
+		event.preventDefault();
+		var d = $(this).serializeObject();
+			d.attendees = JSON.parse(sessionStorage.invitations);
+			d.url = window.location.protocol +'//'+ window.location.hostname +(':'+ window.location.port || '') ;
+		socket.emit('create', d);
+		socket.emit('post', d);
+
+        sessionStorage.userEmail = d.name;
+	});
+
+    // Room created
+	socket.on('created', function (d) {
+		console.log(d);
+
+        var _attendees = d.attendees;
+        for(var i=0; i<=_attendees.length-1; i++){
+        	$('#usersOffline').append('<li>'+_attendees[i]+'</li>');
+        }
+
+		$('.slide_viewport').hide();
+		$('#chat').show();
+	});
+} // create_room
 
 
-
+function join_room(){
 	// LOGIN FOR USER WITH URL EMAIL PARAM
 	$('#form_login').submit(function () {
         var d = $(this).serializeObject();
@@ -76,24 +102,6 @@ $(function(){
         event.preventDefault();
     });
 
-    // CHAT
-    $('#valid_msg').on('click', function(){
-		send();
-	});
-
-
-	socket.on('created', function (d) {
-		console.log(d);
-
-        var _attendees = d.attendees;
-        for(var i=0; i<=_attendees.length-1; i++){
-        	$('#usersOffline').append('<li>'+_attendees[i]+'</li>');
-        }
-
-		$('.slide_viewport').hide();
-		$('#chat').show();
-	});
-
 	socket.on('joined', function (d) {
 
         if(d.success) {
@@ -101,7 +109,9 @@ $(function(){
         	$('#chat').show();
         }
     });
+}
 
+function show_connected_users(){
 	// CHECK ATTENDEES NUMBER
 	socket.on('added', function (d) {
         console.log(d);
@@ -112,14 +122,16 @@ $(function(){
         }
     	$('#usersOnline').prepend('<li>'+d.myself+'</li>');
     });
+}
 
-	// UNVEIL
-	socket.on('unveil', function (d) {
-        console.log(d);
-        $('#unveil').show().append(d.message);
-    });
 
-    // CHAT
+function chat_management(){
+	// Write msg
+    $('#valid_msg').on('click', function(){
+		send();
+	});
+
+    // Get msg
     function send(){
 		var _m = {};
 		_m.msg = $('#user_msg').val();
@@ -130,14 +142,30 @@ $(function(){
 		$('#user_msg').val('');
 	}
 
+	// Keyboard 'enter' to valid msg
 	document.addEventListener('keyup', function(e){
 		if(e.keyCode === 13) send();
 	});
 
+	// Display msg
 	socket.on('receive', function(msg){
 		$('#conversation').prepend('<li><strong>'+msg.author+'</strong><br>'+msg.msg+'</li>');
 	});
+} // chat_management
 
+
+
+function unveil_secret(){
+	// UNVEIL
+	socket.on('unveil', function (d) {
+        console.log(d);
+        $('#unveil').show().append(d.message);
+    });
+}
+
+
+function serialize(){
+	// Convert forms data into JSON
 	$.fn.serializeObject = function(){
 		var o = {};
 		var a = this.serializeArray();
@@ -153,88 +181,83 @@ $(function(){
 		});
 		return o;
 	};
-
-
-
-});
-
-
-var socket = io();
-var emails = !sessionStorage.emails ? null : JSON.parse(sessionStorage.emails) ;
-var url = window.location.search;
-var user_email = url.substring(url.lastIndexOf('=')+1) || 0;
-var clientId = '605751688227-u0fliiq32028oh6uim9mguae8hqfb7jj.apps.googleusercontent.com';
-var apiKey = 'AIzaSyAOpIxefwOLJi7luSaFxNNSjNn7EjJWru0';
-var scopes = 'https://www.googleapis.com/auth/contacts.readonly';
-
-
-
-// Create list of contact
-$.each(emails, function(ind, val){
-    $('#contact_list').append('<li data-index='+ind+'><p>'+val.title+' <span class="user_mail">'+val.email+'</span></p></li>');
-});
+}
 
 
 
 // Auth
 function handleClientLoad() {
-        gapi.client.setApiKey(apiKey);
-        window.setTimeout(checkAuth,1);
+    gapi.client.setApiKey(apiKey);
+    window.setTimeout(checkAuth,1);
+}
+function checkAuth() {
+    gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true}, handleAuthResult);
+}
+function handleAuthResult(authResult) {
+    var authorizeButton = document.getElementById('authorize-button');
+    if (authResult && !authResult.error) {
+
+        authorizeButton.style.visibility = 'hidden';
+        //console.log(authResult);
+
+        getContacts(authResult.access_token);
+
+    } else {
+        authorizeButton.style.visibility = '';
+        authorizeButton.onclick = handleAuthClick;
     }
-    function checkAuth() {
-        gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true}, handleAuthResult);
-    }
-    function handleAuthResult(authResult) {
-        var authorizeButton = document.getElementById('authorize-button');
-        if (authResult && !authResult.error) {
+}
+function handleAuthClick(event) {
+    gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: false}, handleAuthResult);
+    return false;
+}
 
-            authorizeButton.style.visibility = 'hidden';
-            //console.log(authResult);
+function getContacts(token){
+    var url = 'https://www.google.com/m8/feeds/contacts/default/full/'
+    + '?access_token='+token+'&alt=json&max-results=1000';
 
-            getContacts(authResult.access_token);
+    $.get(url, function(data){
+        console.log(data);
 
-        } else {
-            authorizeButton.style.visibility = '';
-            authorizeButton.onclick = handleAuthClick;
-        }
-    }
-    function handleAuthClick(event) {
-        gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: false}, handleAuthResult);
-        return false;
-    }
+        var _tmp = [];
 
-    function getContacts(token){
-        var url = 'https://www.google.com/m8/feeds/contacts/default/full/'
-        + '?access_token='+token+'&alt=json&max-results=1000';
+        //sessionStorage.setItem('emails', JSON.stringify(data.feed.entry));
 
-            $.get(url, function(data){
-                console.log(data);
-
-                var _tmp = [];
-
-                //sessionStorage.setItem('emails', JSON.stringify(data.feed.entry));
-
-                $.each(data.feed.entry, function(ind, val){
-                    if(val['gd$email']){
-                        _tmp.push({
-                            title : val['title'].$t,
-                            email : val['gd$email'][0].address
-                        });
-
-
-                        /*$('ul').append('<li><p>'+val['title'].$t+'<br>'+val['gd$email'][0].address+'</p></li>');*/
-                    }
-
-                    if(ind === data.feed.entry.length-1){
-                        sessionStorage.setItem('emails', JSON.stringify(_tmp));
-                        $('.steps li:first-child').addClass('done');
-                        $(document).trigger('elDone');
-                        $('.connected').fadeIn(100);
-                        //window.location = '/login';
-                    }
+        $.each(data.feed.entry, function(ind, val){
+            if(val['gd$email']){
+                _tmp.push({
+                    title : val['title'].$t,
+                    email : val['gd$email'][0].address
                 });
+
+
+                /*$('ul').append('<li><p>'+val['title'].$t+'<br>'+val['gd$email'][0].address+'</p></li>');*/
+            }
+
+            if(ind === data.feed.entry.length-1){
+                sessionStorage.setItem('emails', JSON.stringify(_tmp));
+                $('.steps li:first-child').addClass('done');
+                $(document).trigger('elDone');
+                $('.connected').fadeIn(100);
+                //window.location = '/login';
+            }
         });
-    }
+    });
+}
 
 
+// Initalize
+$(function(){
 
+	$('#chat').hide();
+	$('#unveil').hide();
+
+	serialize();
+	contact_management();
+	chat_management();
+	create_room();
+	join_room();
+	show_connected_users();
+	unveil_secret();
+
+});
